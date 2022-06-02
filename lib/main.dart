c4d969407e30e18fcdede9e38ca486df8a992ee2
Fernando Ferracini <fernandoferracini@gmail.com>
2022-06-02 17:40:27 -0300

    02/06/2022 17:40

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,17 @@
-import 'package:bytebank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
+import 'package:bytebank/screens/dashboard.dart';
 
-class DarkModeModel extends Model {
-  bool _darkmode = false;
-
-  void toggle() {
-    _darkmode = !_darkmode;
-    debugPrint('Darkmode is now $_darkmode');
-    notifyListeners();
-  }
+void main() {
+  runApp(BytebankApp());  
 }
-
-void main() => runApp(
-      BytebankApp(),
-    );
 
 class BytebankApp extends StatelessWidget {
-  const BytebankApp({
-    Key key,
-  }) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<DarkModeModel>(
-      model: DarkModeModel(),
-      child: ScopedModelDescendant<DarkModeModel>(
-        builder: (context, child, model) {
-          return MaterialApp(
-            theme: model._darkmode ? ThemeData.dark() : buildThemeData(),
-            //theme: ThemeData.dark(),
-            home: Listatransferencia(),
-          );
-        }
-      ),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Dashboard(),
     );
   }
-
-  ThemeData buildThemeData() {
-    return ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ));
-  }
 }
-//Crt + alt + L quebra o codigo nas virgulas
-//alt + enter dá as opções
